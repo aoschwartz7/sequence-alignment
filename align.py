d8e5bcf4a04e9574f1b762c9e098146baafe8e81
alec <aoschwartz7@gmail.com>
2020-12-17 14:39:18 -0500

    added global alignment function with score_only parameter

--- align.py
+++ align.py
@@ -6,6 +6,8 @@ from Bio.pairwise2 import format_alignment
 from itertools import combinations
 import random
 
+from Bio import Align
+
 pd.options.mode.chained_assignment = None  # TODO
 df = pd.read_csv("data.csv") # import sequence data
 
@@ -52,13 +54,10 @@ def makeSequences(df:pd.DataFrame, attr:str, sequence:str):
         eachSequence = frames[i][sequence].tolist() # combine Combos into single sequence
         eachSequence = "".join(frames[i][sequence].tolist()) # combine Combos into single sequence
         sequencesDict[sequenceName] = eachSequence
-    return sequencesDict
+        sequencesList = list(sequencesDict.values()) # for quick test i'm using this list
 
-        #### FOLLOWING FUNCTIONS ARE FOR MULTIPLE ALIGNMENTS #####
+    return sequencesDict, sequencesList
 
-
-
-        #### FOLLOWING FUNCTIONS ARE FOR PAIRWISE ALIGNMENTS #####
 def makePairs(sequencesDict:dict):
     """ Make Combo sequence pairs for desired frames/attributes for pairwise-alignments.
     Args:
@@ -91,21 +90,35 @@ def makeTuples(sequencesList:list, sequencesDict:dict):
         pairNames.append((first,second))
     return pairs, pairNames
 
-
-def multipleSequenceAlign():
-    """
-    """
-
 def globalPairwiseAlign(pairs:list, pairNames:list):
     """ Finds global alignment between pairs, applying
-    Needleman-Wunsch algorithm.Function comes from:
+    Needleman-Wunsch algorithm (I think). Function comes from:
     https://biopython.org/docs/1.75/api/Bio.pairwise2.html
-
+    Function parameters:
+        - Do a global alignment.
+        - Identical characters are given 2 points
+        - 1 point is deducted for each non-identical character.
+        - 0.5 points are deducted when opening a gap
+        - 0.1 points are deducted when extending it.
     Args:
         frames (list): List of separate Prep DataFrames
     Returns:
         results...
     """
+    aligner = Align.PairwiseAligner()
+    aligner.mode = 'local'
+    # testing ASCII compatibility
+    # alignments = pairwise2.align.globalms(sequencesList[0], sequencesList[1], 2, -1, -.5, -.1)
+    alignments = pairwise2.align.globalms(sequencesList[2], sequencesList[3], 2, -1, -.5, -.1, score_only=True)
+    print(alignments)
+    # x = 0
+    # for alignment in sorted(alignments):
+    #     if x==2:
+    #         break
+    #     print("Score = {}".format(alignment.score))
+    #     x+=1
+
+
 # def globalAlign(results, seq_pair, seq1, seq2):
 #     """ Perform global alignment on sequence combinations and
 #     https://biopython.org/docs/1.75/api/Bio.pairwise2.html
@@ -134,9 +147,8 @@ def globalPairwiseAlign(pairs:list, pairNames:list):
     # generate each Combo sequence per Prep as key-value pair
 
 
-
-
 if __name__ == '__main__':
     df = mapASCII(df)
-    sequencesDict = makeSequences(df, 'Prep', 'Sequence')
+    sequencesDict, sequencesList = makeSequences(df, 'Prep', 'Sequence')
     pairs, pairNames = makePairs(sequencesDict)
+    globalPairwiseAlign(pairs, pairNames)
